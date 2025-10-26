5a82fff7ef373a1cf9fb2fa19c882d42b0ca85f3
Antônio Neves <126279366+Neeveszr@users.noreply.github.com>
2025-10-26 14:19:50 -0300

    Delete infra-prova-pratica directory

--- infra-prova-pratica/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Nome do bucket criado"
-  value       = aws_s3_bucket.app_bucket.bucket
-}
-
-output "iam_user" {
-  description = "Usuário IAM criado para a prova"
-  value       = aws_iam_user.lab_user.name
-}
